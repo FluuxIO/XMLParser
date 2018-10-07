27219a02623c205f03429cead47d012b8e3b3d14
Mickael Remond <mremond@process-one.net>
2018-10-07 20:26:37 +0200

    Initial Commit

---
+++ XMLParser/XMLParser.h
@@ -0,0 +1,19 @@
+//
+//  XMLParser.h
+//  XMLParser
+//
+//  Created by Mickaël Rémond on 07/10/2018.
+//  Copyright © 2018 ProcessOne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XMLParser.
+FOUNDATION_EXPORT double XMLParserVersionNumber;
+
+//! Project version string for XMLParser.
+FOUNDATION_EXPORT const unsigned char XMLParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XMLParser/PublicHeader.h>
+
+
